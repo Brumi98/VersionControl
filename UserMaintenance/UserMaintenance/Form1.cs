167d86b462c596e3da0040eee9546b6cce761eee
Ádám Kappéter <adam@kappeterdesign.hu>
2020-09-23 11:45:31 +0200

    második

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
@@ -15,6 +16,10 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lblLastName.Text = ResourceFile_1.LastName;
+            lblFirstName.Text = ResourceFile_1.FirstName;
+            btnAdd.Text = ResourceFile_1.Add;
+
         }
     }
 }
