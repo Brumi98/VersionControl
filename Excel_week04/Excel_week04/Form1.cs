660927e41081247ceb47e7e18697365b4020f3f7
Ádám Kappéter <adam@kappeterdesign.hu>
2020-10-03 12:14:04 +0200

    7-es feladat

--- Excel_week04/Excel_week04/Form1.cs
+++ Excel_week04/Excel_week04/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 using System.Data.Entity.Migrations.Model;
+using System.Drawing.Text;
+using System.Data.Entity;
 
 namespace Excel_week04
 {
@@ -45,7 +47,7 @@ namespace Excel_week04
 
                 xlSheet = xlWB.ActiveSheet;
 
-                //CreateTable();
+                CreateTable();
 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
@@ -60,10 +62,72 @@ namespace Excel_week04
                 xlWB = null;
                 xlApp = null;
 
+            }   
+        }
+        private void CreateTable()
+        {
+
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+                xlSheet.Cells[1, 2] = headers[1];
+                xlSheet.Cells[1, 3] = headers[2];
+                xlSheet.Cells[1, 4] = headers[3];
+                xlSheet.Cells[1, 5] = headers[4];
+                xlSheet.Cells[1, 6] = headers[5];
+                xlSheet.Cells[1, 7] = headers[6];
+                xlSheet.Cells[1, 8] = headers[7];
+                xlSheet.Cells[1, 9] = headers[8];
             }
 
+            object[,] values = new object[Flats.Count, headers.Length];
 
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
 
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "="+GetCell(counter + 2, 7).ToString() + "*" + GetCell(counter + 2, 8).ToString();
+                counter++;
+            }
+            xlSheet.get_Range(
+                        GetCell(2, 1),
+                        GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
 
         }
 
