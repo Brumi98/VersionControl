2ccfc533a3683cd766187f8c2e9d27641e5d0cc8
Ádám Kappéter <adam@kappeterdesign.hu>
2020-10-14 18:55:06 +0200

    Kész

---
+++ Websupport/Websupport/Entities/RateData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Websupport.Entities
+{
+    public class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
+
+        public BindingList<RateData> Rates;
+
+    }
+}
--- Websupport/Websupport/Form1.cs
+++ Websupport/Websupport/Form1.cs
@@ -7,14 +7,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
+using Websupport.Entities;
+using Websupport.MnbServiceReference;
 
 namespace Websupport
 {
     public partial class Form1 : Form
     {
+        public BindingList<RateData> Rates = new BindingList<RateData>();
+        public BindingList<string> Currencies = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
+      
+
+            //var mnbService = new MNBArfolyamServiceSoapClient();
+
+            //var request = new GetCurrenciesRequestBody();
+
+            //var response = mnbService.GetCurrencies(request);
+
+            //var result = response.GetCurrenciesResult;
+            //comboBox1.DataSource = Currencies;
+
+            RefreshData();
+
+        }
+        public void RefreshData()
+        {
+            this.Rates.Clear();
+            
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+
+            dataGridView1.DataSource = Rates;
+            var result = response.GetExchangeRatesResult;
+            Dgrbet();
+            XMLfeldolg(result);
+
+        }
+
+        private void Dgrbet()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void XMLfeldolg(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                if (childElement == null) continue;
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
