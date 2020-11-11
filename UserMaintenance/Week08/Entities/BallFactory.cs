e82492df410c0365a95ffadb73502c8c4677ffde
Ádám Kappéter <adam@kappeterdesign.hu>
2020-11-11 11:53:06 +0100

    3 feladatig

---
+++ UserMaintenance/Week08/Entities/BallFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week08.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
