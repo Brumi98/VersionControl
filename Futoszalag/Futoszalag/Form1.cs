b6d787ac4183aa65eb70559b3312773327ba175e
Ádám Kappéter <adam@kappeterdesign.hu>
2020-11-11 17:56:21 +0100

    Kész feladat

--- Futoszalag/Futoszalag/Form1.cs
+++ Futoszalag/Futoszalag/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Futoszalag.Entities;
+﻿using Futoszalag.Abstractions;
+using Futoszalag.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,45 +14,90 @@ namespace Futoszalag
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
+        
+        private Toy _nextToy;
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            Factory = new CarFactory();
         }
 
         private void createTimer_Tick_1(object sender, EventArgs e)
         {
-                var ball = Factory.CreateNew();
-                _balls.Add(ball);
-                ball.Left = -ball.Width;
-                mainPanel.Controls.Add(ball);
+                var toy = Factory.CreateNew();
+                _toys.Add(toy);
+                toy.Left = -toy.Width;
+                mainPanel.Controls.Add(toy);
             }
 
         private void conveyorTimer_Tick_1(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveBall();
-                if (ball.Left > maxPosition)
-                    maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
+        }
+
+        private void btnCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory()
+            {
+                BallColor = btnColor.BackColor
+            };
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+            {
+                Controls.Remove(_nextToy);
+            }
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
+
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            button.BackColor = colorPicker.Color;
         }
     }
 }
