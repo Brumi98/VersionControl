7632fef068afb5e3fc7cbd19285493fd1e1579f6
Ádám Kappéter <adam@kappeterdesign.hu>
2020-11-18 15:02:15 +0100

    4 és fél feladat

---
+++ Unit testing/UnitTestExample.Test/AccountControllerTestFixure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestExample.Controllers;
+using UnitTestExample.Abstractions;
+using UnitTestExample.Test;
+
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixure
+    {
+        public AccountControllerTestFixure()
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+          
+            //,,,
+
+        }
+
+
+
+    }
+}
